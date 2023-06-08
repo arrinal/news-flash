f42fa902803b6b5fac3498de66e84c15cf6a5640
Arrinal S <arrinalsf@gmail.com>
2023-06-08 23:23:08 +0700

    added project

---
+++ News Flash/News_FlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_FlashApp.swift
+//  News Flash
+//
+//  Created by Arrinal Sholifadliq on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct News_FlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
